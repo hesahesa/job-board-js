95cfc7a7636ac9cc71d2f6258a1f4950ba36317f
hesahesa <prahesa@yahoo.com>
2025-05-01 01:21:45 +0700

    update job interface

--- utils/model/job.ts
+++ utils/model/job.ts
@@ -1,8 +1,10 @@
 export interface Job {
-  id: number;
-  title: string;
-  description: string;
-  created_by: string;
+  company_name: string | null;
   created_at: string;
-  updated_at?: string;
+  created_by: string | null;
+  description: string | null;
+  id: number;
+  job_type: string | null;
+  location: string | null;
+  title: string | null;
 }
