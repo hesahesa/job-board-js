681458ec933c03a1b04eea717e2a2cd560dbd848
hesahesa <prahesa@yahoo.com>
2025-05-01 10:07:54 +0700

    make homepage wider

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const jobs = await getAllJobs();
   return (
     <>
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      <main className="flex-1 flex w-full flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Available Jobs:</h2>
         <JobPostList jobs={jobs}/>
       </main>
