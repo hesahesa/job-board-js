c0a61eba8f95cd0016537fde08a6f48019dcc680
hesahesa <prahesa@yahoo.com>
2025-05-01 15:41:30 +0700

    add delete job page

--- app/actions.ts
+++ app/actions.ts
@@ -4,7 +4,7 @@ import { encodedRedirect } from "@/utils/utils";
 import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { insertJob, updateJobById } from "@/utils/supabase/queries";
+import { deleteJobById, insertJob, updateJobById } from "@/utils/supabase/queries";
 
 export const signUpAction = async (formData: FormData) => {
   const email = formData.get("email")?.toString();
@@ -197,6 +197,31 @@ export const updateJobAction = async (formData: FormData) => {
 
   if (!updated) {
     return encodedRedirect("error", `/dashboard/edit-job/${id}`, "Failed to update job");
+  }
+
+  return redirect(`/dashboard`);
+};
+
+export const deleteJobAction = async (formData: FormData) => {
+  const id = formData.get("id")?.toString();
+
+  if (!id) {
+    return encodedRedirect("error", `/dashboard/delete-job/${id}`, "All fields are required");
+  }
+
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return encodedRedirect("error", "/sign-in", "You must be signed in to update a job");
+  }
+
+  const updated = deleteJobById(Number.parseInt(id));
+
+  if (!updated) {
+    return encodedRedirect("error", `/dashboard/delete-job/${id}`, "Failed to update job");
   }
 
   return redirect(`/dashboard`);
--- app/dashboard/delete-job/[id]/page.tsx
+++ app/dashboard/delete-job/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { updateJobAction } from "@/app/actions";
+import { deleteJobAction } from "@/app/actions";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -17,27 +17,23 @@ export default async function EditJob({
   }
 
   return (
-    <form className="flex flex-col min-w-64 max-w-64 mx-auto" action={updateJobAction}>
-      <h1 className="text-2xl font-medium">Edit Job</h1>
+    <form className="flex flex-col min-w-64 max-w-64 mx-auto" action={deleteJobAction}>
+      <h1 className="text-2xl font-medium">Delete Job</h1>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
         <input type="hidden" name="id" value={job.id} />
         <Label htmlFor="title">Title</Label>
-        <Input name="title" placeholder="My Awesome Job" defaultValue={job.title?? ""} required />
+        <Input name="title" placeholder="My Awesome Job" defaultValue={job.title?? ""} required readOnly/>
         <Label htmlFor="company_name">Company Name</Label>
-        <Input name="company_name" placeholder="XYZ Inc." defaultValue={job.company_name?? ""} required />
+        <Input name="company_name" placeholder="XYZ Inc." defaultValue={job.company_name?? ""} required readOnly/>
         <Label htmlFor="description">Description</Label>
-        <Input name="description" placeholder="XYZ Inc. is an awesome company and we want you!" defaultValue={job.description?? ""} required />
+        <Input name="description" placeholder="XYZ Inc. is an awesome company and we want you!" defaultValue={job.description?? ""} required readOnly/>
         <Label htmlFor="location">Location</Label>
-        <Input name="location" placeholder="Antarctica" defaultValue={job.location?? ""} required />
+        <Input name="location" placeholder="Antarctica" defaultValue={job.location?? ""} required readOnly/>
         <Label htmlFor="job_type">Job Type</Label>
-        <select name="job_type" id="job_type" defaultValue={job.job_type ?? "full_time"}>
-          <option value="full_time">Full-Time</option>
-          <option value="part_time">Part-Time</option>
-          <option value="contract">Contract</option>
-        </select>
+        <Input name="location" placeholder="Antarctica" defaultValue={job.job_type?? ""} required readOnly/>
         <br />
-        <SubmitButton pendingText="Submitting...">
-          Submit
+        <SubmitButton variant={"destructive"} pendingText="Submitting...">
+          Delete
         </SubmitButton>
       </div>
     </form>
